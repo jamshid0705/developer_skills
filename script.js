7848877c15b3c39eb3432ee2e95493d7f2c75ec3
jamshid <jamshidshamshod0705@gmail.com>
2023-02-22 13:47:29 +0500

    debugger

--- script.js
+++ script.js
@@ -1,6 +1,53 @@
 // Remember, we're gonna use strict mode in all scripts now!
 "use strict";
 
-const a = "8";
-const age = (a) => a;
+// const a = "8";
+// const age = (a) => a;
 
+/////////// Coding //////////////
+// function sleepIn(weekday, vacation) {
+//   if (weekday == true && vacation == true) {
+//     return true;
+//   } else if (weekday == true && vacation == false) {
+//     return false;
+//   } else if (weekday == false && vacation == true) {
+//     return false;
+//   } else {
+//     return true;
+//   }
+// }
+////////
+// function sumDouble(a, b) {
+//   if (a === b) {
+//     return 2 * (a + b);
+//   } else {
+//     return a + b;
+//   }
+// }
+
+//////////////////// 59 using google, stackoverflow and MDH ////////////////
+
+// const temp = [3, 3, 2, 1, "error", 3, 13, 17, 15, 14, 9, 5];
+
+// const calcTemp = function (temp) {
+//   let max = 0;
+//   let min = 0;
+//   for (let i = 0; i < temp.length; i++) {
+//     if (typeof temp[i] !== "number") continue;
+//     debugger
+//     if (temp[i] >= max) {
+//       max = temp[i];
+//     } else if (temp[i] <= min) {
+//       min = temp[i];
+//     }
+//   }
+//   console.log(max);
+//   console.log(min);
+// };
+
+// calcTemp(temp);
+
+// const a = [1, 2, 3];
+// const b = [4, 5, 6];
+// const c = a.concat(b);
+// console.log(c);
